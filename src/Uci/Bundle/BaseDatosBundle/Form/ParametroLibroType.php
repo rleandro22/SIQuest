dd981badc8b0647d46c6a29121d4fdee9d4e1b9a
rleandro <rleandro@uci1493.ucinetwork.com>
2015-11-09 16:59:41 -0600

    Nada

---
+++ src/Uci/Bundle/BaseDatosBundle/Form/ParametroLibroType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Uci\Bundle\BaseDatosBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ParametroLibroType extends AbstractType {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add('libro', 'entity', array('class' => 'UciBaseDatosBundle:Libro', 'required' => false, 'empty_value' => 'Libro'))
+                ->add('porcentaje', 'integer', array("label" => "Página Num.: ", "mapped" => false, "required" => false, 'precision' => 0, 'constraints' => array(
+                        new Regex(array('pattern' => '/^[0-9]\d*$/', 'message' => 'Use solo números positivos.')))))
+                ->add('numeroPagina', 'integer', array("label" => "Página Num.: ", "mapped" => false, "required" => false, 'precision' => 0, 'constraints' => array(
+                        new Regex(array('pattern' => '/^[0-9]\d*$/', 'message' => 'Use solo números positivos.')))));
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'Uci\Bundle\BaseDatosBundle\Entity\Pregunta'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return 'uci_bundle_basedatosbundle_capitulo';
+    }
+
+}
